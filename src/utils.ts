f4de9d4c23e83653e084207da57dfe4903e7c7ab
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-08-04 04:21:02 +0700

    fix: Allow `.` in key name

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,3 @@
 export const toKeyname = (namespace: string | null, key: string) => `name::${namespace ?? 'default'}::key::${key}`;
 
-export const isKeyValid = (key: string) => /^[a-z0-9\-_]{1,64}$/i.test(key);
+export const isKeyValid = (key: string) => /^[a-z0-9\-_.]{1,64}$/i.test(key);
