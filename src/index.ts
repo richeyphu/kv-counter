6c02b5d9dca8e20afcbd2c5435d26469b1442169
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-08-04 04:52:36 +0700

    feat: Add CORS middleware to allow cross-origin requests

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import { Router } from 'worktop';
+import { Router, compose } from 'worktop';
 import { start } from 'worktop/cfw';
 import * as Cache from 'worktop/cfw.cache';
+import * as CORS from 'worktop/cors';
 import * as Route from './routes';
 
 import type { Context } from './types';
 
 const API = new Router<Context>();
 
-API.prepare = Cache.sync();
+API.prepare = compose(
+	CORS.preflight({
+		origin: '*', // allow any `Origin` to connect
+		headers: ['Cache-Control', 'Content-Type', 'X-Count'],
+		methods: ['GET', 'OPTIONS'],
+	}),
+	Cache.sync()
+);
 
 API.add('GET', '/', Route.index);
 API.add('GET', '/hit/:namespace?/:key', Route.hit);
